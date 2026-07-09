b466adcef582cecfba2b008c7ef64561e92d8604
A-maina <4lwachira@gmail.com>
2026-07-09 10:20:14 +0300

    Configure Flask backend and MySQL integration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from sqlalchemy import text
 
 from app.config.config import Config
 from app.extensions import db, migrate, bcrypt, jwt
+
 from app import models
 from app.models.role import Role
 from app.models.user import User
@@ -14,9 +16,16 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     jwt.init_app(app)
+    with app.app_context():
+     try:
+        db.session.execute(text("SELECT 1"))
+        print("✅ Database connection successful.")
+     except Exception as e:
+        print(f"❌ Database connection failed: {e}")
 
     @app.route("/")
     def home():
--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy.engine import URL
 
 load_dotenv()
 
@@ -7,15 +8,15 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:"
-        f"{os.getenv('DB_PORT')}/"
-        f"{os.getenv('DB_NAME')}"
+    SQLALCHEMY_DATABASE_URI = URL.create(
+        drivername="mysql+pymysql",
+        username=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        database=os.getenv("DB_NAME"),
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    print("DB_USER:", os.getenv("DB_USER"))
